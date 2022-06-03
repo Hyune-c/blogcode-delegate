785db427464ed7ed9a18737e6569038c38292368
hyune <choi8608@gmail.com>
2022-06-04 02:59:54 +0900

    Initial commit

---
+++ src/main/java/com/example/blogcodedelegate/BlogcodeDelegateApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogcodedelegate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogcodeDelegateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogcodeDelegateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blogcodedelegate/BlogcodeDelegateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogcodedelegate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogcodeDelegateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
