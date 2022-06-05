0eeae17451cfa244443d8c56016095cb93af5561
hyune <choi8608@gmail.com>
2022-06-05 12:04:34 +0900

    [step4] 사용 코드

--- src/main/java/com/example/blogcodedelegate/SomethingWithExcelService.java
+++ src/main/java/com/example/blogcodedelegate/SomethingWithExcelService.java
@@ -9,18 +9,22 @@ import java.io.File;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ExcelService {
+public class SomethingWithExcelService {
 
 	private final AbstractAmazonS3UploadRequestProvider amazonS3DelegateProvider;
 	private final AbstractAmazonS3Service amazonS3Delegator;
 
-	public ExcelService(final AmazonS3CommonPrivateUploadRequestProvider amazonS3DelegateProvider, final AmazonS3PrivateService amazonS3Delegator) {
+	public SomethingWithExcelService(final AmazonS3CommonPrivateUploadRequestProvider amazonS3DelegateProvider, final AmazonS3PrivateService amazonS3Delegator) {
 		this.amazonS3DelegateProvider = amazonS3DelegateProvider;
 		this.amazonS3Delegator = amazonS3Delegator;
 	}
 
-	public void Upload(final File file) {
+	public void doSomething(final File file) {
+		// 전처리 ...
+
 		final PutObjectRequest request = amazonS3DelegateProvider.createRequest(file);
 		amazonS3Delegator.putObject(request);
+
+		// 후처리 ...
 	}
 }
